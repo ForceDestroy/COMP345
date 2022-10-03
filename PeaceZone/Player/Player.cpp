ed28264995203f2805ef8413666f1fb6c216a06c
JaiyD <47201179+JaiyD@users.noreply.github.com>
2022-10-03 15:00:43 -0400

    Adding comments and osstream

--- PeaceZone/Player/Player.cpp
+++ PeaceZone/Player/Player.cpp
@@ -1,17 +1,27 @@
 #include "Player.h"
 
+//Default constructor
 Player::Player()
 {
     territories = new std::vector<Territory*>();
     handOfCards = new std::vector<Cards*>();
-    orders = new std::vector<Orders*>();
+    listOfOrders = new std::vector<Orders*>();
 }
 
+//Parameterized constructor
+Player::Player(std::vector<Territory*>* territories, std::vector<Cards*>* handOfCards, std::vector<Orders*>* listOfOrders)
+{
+    this->territories = territories;
+    this->handOfCards = handOfCards;
+    this->listOfOrders = listOfOrders;
+}
+
+//Destructor
 Player::~Player()
 {
     delete territories;
     delete handOfCards;
-    delete orders;
+    delete listOfOrders;
 }
 
 //copy constructor
@@ -19,7 +29,8 @@ Player::Player(const Player& player)
 {
     territories = new std::vector<Territory*>();
     handOfCards = new std::vector<Cards*>();
-    orders = new std::vector<Orders*>();
+    listOfOrders = new std::vector<Orders*>();
+    //Copying the values into the new object
     for (Territory* territory : *player.territories)
     {
         territories->push_back(territory);
@@ -28,24 +39,25 @@ Player::Player(const Player& player)
     {
         handOfCards->push_back(card);
     }
-    for (Orders* order : *player.orders)
+    for (Orders* order : *player.listOfOrders)
     {
-        orders->push_back(order);
+        listOfOrders->push_back(order);
     }
 }
 
+//Attack method
 void Player::toAttack()
 {
     std::cout << "Player is attacking" << std::endl;
     // get adjacent enemy territories
 }
-
+//Defend method
 void Player::toDefend()
 {
     std::cout << "Player is defending" << std::endl;
     // get adjacent friendly territories
 }
-
+//Issue order method
 void Player::issueOrder()
 {
     std::cout << "Player is issuing order" << std::endl;
@@ -58,7 +70,31 @@ Player& Player::operator=(const Player& p)
     {
         territories = p.territories;
         handOfCards = p.handOfCards;
-        orders = p.orders;
+        listOfOrders = p.listOfOrders;
     }
     return *this;
+}
+
+//osstream operator
+std::ostream& operator<<(std::ostream& os, const Player& p)
+{
+    os << "Player's territories: " << std::endl;
+    for (Territory* territory : *p.territories)
+    {
+        //os << *territory << std::endl;
+        // Waiting on Territories << overload
+    }
+    os << "Player's cards: " << std::endl;
+    for (Cards* card : *p.handOfCards)
+    {
+        //os << *card << std::endl;
+        // Waiting on Card << overload
+    }
+    os << "Player's listOfOrders: " << std::endl;
+    for (Orders* order : *p.listOfOrders)
+    {
+        //os << *order << std::endl;
+        // Waiting on Orders << overload
+    }
+    return os;
 }
--- PeaceZone/Player/Player.h
+++ PeaceZone/Player/Player.h
@@ -7,22 +7,28 @@
 #define new new( _NORMAL_BLOCK , __FILE__ , __LINE__ )
 #endif
 
+//TODO Add comments to explain the class
+//TODO change these to include headers later
 class Territory;
 class Cards;
 class Orders;
 
+//Player class
 class Player
 {
-    private:
+private:
     std::vector<Territory*>* territories;
     std::vector<Cards*>* handOfCards;
-    std::vector<Orders*>* orders;
-    public:
-        Player();
-        ~Player();
-        Player(const Player& player);
-        Player& operator=(const Player& p);
-        void toAttack();
-        void toDefend();
-        void issueOrder();
+    std::vector<Orders*>* listOfOrders;
+public:
+    Player();
+    Player(std::vector<Territory*>* territories, std::vector<Cards*>* handOfCards, std::vector<Orders*>* listOfOrders);
+    Player(const Player& player);
+    ~Player();
+    Player& operator=(const Player& p);
+    void toAttack();
+    void toDefend();
+    void issueOrder();
+    
+    friend std::ostream& operator<<(std::ostream& out, const Player& p);
 };
