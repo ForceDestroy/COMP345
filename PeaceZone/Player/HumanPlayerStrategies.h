132ce0300ed427c969c1360fb9f93f066c8c1051
MinhDuc1711 <99374119+MinhDuc1711@users.noreply.github.com>
2022-12-02 18:30:02 -0500

    Add files via upload

--- PeaceZone/Player/HumanPlayerStrategies.h
+++ PeaceZone/Player/HumanPlayerStrategies.h
@@ -5,17 +5,24 @@
 #include "../GameEnginer/GameEngine.h"
 class HumanPlayerStrategy : public PlayerStrategy {
 public:
+	// terToAttack and terToDefend stores an appropriate territory every time an order is issued
 	std::vector<Territory*> terToAttack;
 	std::vector<Territory*> terToDefend;
+	// Constructors
 	HumanPlayerStrategy();
 	HumanPlayerStrategy(HumanPlayerStrategy* hps);
+	// Destructors
 	~HumanPlayerStrategy();
-	char getOption();
+	// Get territory by name
+	Territory* getTerByName(Player* player, std::string name);
+	// Get name of an owned territory
 	std::string getName(Player* player);
-	bool isAPlayer(GameEngine* ge, std::string name);
+	// Get number of troops
 	int getTroops(Player* player, int troopLim);
+	// Get the action of the player
+	char getOption();
+	// toAttack and toDefend are only decided after a player has finish issuing order since orders are manually input
 	std::vector<Territory*> toAttack(Player* player) override;
 	std::vector<Territory*> toDefend(Player* player) override;
 	void issueOrder(Player* player, GameEngine* ge);
-	Territory* getTerByName(Player* player, std::string name);
 };
