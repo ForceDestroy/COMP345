6444bb0a68fe2c4eead5b20314e3e477e3d5ae7f
MinhDuc1711 <99374119+MinhDuc1711@users.noreply.github.com>
2022-12-04 22:41:31 -0500

    Add files via upload

---
+++ PeaceZone/PlayerStrategiesDriver.cpp
@@ -0,0 +1,21 @@
+#include "../GameEngine/GameEngine.h"
+inline void testComputerStrategies(GameEngine *gameEngine) 
+{
+    std::string input;
+
+    //Ask the user which mode to read the commands from
+    gameEngine->chooseInputMode();
+
+
+    gameEngine->startupPhase();
+
+    std::cout << "End of startup phase. " << std::endl << std::endl;
+
+    gameEngine->playerList.at(0)->strategy = new CheaterPlayerStrategy();
+    gameEngine->playerList.at(1)->strategy = new BenevolentPlayerStrategy();
+    gameEngine->playerList.at(2)->strategy = new AggressivePlayerStrategy();
+    gameEngine->playerList.at(3)->strategy = new HumanPlayerStrategy();
+
+    gameEngine->mainGameLoop();
+
+}
